a4a2670250b28e5d823317de3279f879df690808
Li Xie <li_xie1992@hotmail.com>
2019-09-06 21:08:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0 
   
   while counter < array.length 
-    array[counter] = array[counter] ** 2
+    puts array[counter] = array[counter] ** 2
     counter += 1
   end
 end
